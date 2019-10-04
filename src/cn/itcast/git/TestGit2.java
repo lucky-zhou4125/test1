bca9a078439f8aceee508866aace578a338568d1
lucky-zhou4125 <zhougang.1988@163.com>
2019-10-04 17:31:08 +0800

    2019年10月4日17:29:48 测试提交

---
+++ src/cn/itcast/git/TestGit2.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+/**
+ * @Discription
+ * @Author rio
+ * @Date 2019/10/04 17:28
+ */
+public class TestGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("仙剑奇侠传");
+    }
+}
