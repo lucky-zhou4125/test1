b3b0f355090335df598ae5901ae7c3f8b5313551
lucky-zhou4125 <zhougang.1988@163.com>
2019-10-04 17:46:28 +0800

    2019年10月4日17:29:48 测试提交2

---
+++ src/cn/itcast/git/TestGit3.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+/**
+ * @Discription
+ * @Author rio
+ * @Date 2019/10/04 17:34
+ */
+public class TestGit3 {
+
+    public static void main(String[] args) {
+        System.out.println("李逍遥 赵灵儿 林月如");
+    }
+}
