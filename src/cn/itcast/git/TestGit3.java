8921fe32f48d376fae4c7776e2b7fe4ac4205ce6
lucky-zhou4125 <zhougang.1988@163.com>
2019-10-05 16:23:11 +0800

    测试冲突

--- src/cn/itcast/git/TestGit3.java
+++ src/cn/itcast/git/TestGit3.java
@@ -1,5 +1,7 @@
 package cn.itcast.git;
 
+import java.util.HashMap;
+
 /**
  * @Discription
  * @Author rio
@@ -8,6 +10,12 @@ package cn.itcast.git;
 public class TestGit3 {
 
     public static void main(String[] args) {
-        System.out.println("11111");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        hashMap.put("key3", "value3");
+
+        System.out.println(hashMap);
     }
 }
