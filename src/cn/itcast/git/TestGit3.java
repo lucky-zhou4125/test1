a15d1251ebca5bb45afa94c6bbdd455c0c7cca30
lucky-zhou4125 <34208512+lucky-zhou4125@users.noreply.github.com>
2019-10-05 16:21:04 +0800

    测试冲突3

--- src/cn/itcast/git/TestGit3.java
+++ src/cn/itcast/git/TestGit3.java
@@ -8,6 +8,12 @@ package cn.itcast.git;
 public class TestGit3 {
 
     public static void main(String[] args) {
-        System.out.println("11111");
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        hashMap.put("key3", "value3");
+         hashMap.put("key4", "value4");
+
+        System.out.println(hashMap);
     }
 }
